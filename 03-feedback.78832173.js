21ad0f183dbf644c2f0f82b5ff6a4a8b132d4ac2
MarushevskyiIvan <131617971+MarushevskyiIvan@users.noreply.github.com>
2023-09-09 18:52:10 +0000

    Deploying to gh-pages from @ MarushevskyiIvan/goit-js-hw-08@987485e69a51fbf770e11712838cce27d973ffa5 🚀

---
+++ 03-feedback.78832173.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("submit",(function(e){e.preventDefault();const t={};return new FormData(e.currentTarget).forEach(((e,r)=>{t[r]=e})),t}));
+//# sourceMappingURL=03-feedback.78832173.js.map

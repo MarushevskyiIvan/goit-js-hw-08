4736492df9123320c649ffa69df11f4599f48ce8
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2023-09-09 23:41:23 +0300

    3.1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,42 @@
 const formEl = document.querySelector('.feedback-form');
+const labelEl = document.querySelector('input');
+const textareaEL = document.querySelector('textarea');
 
-formEl.addEventListener('submit', onInputValue);
+formEl.addEventListener('input', onFormInput);
+
+function onFormInput(evt) {
+  const value = onInputValue(evt);
+
+  localStorFn(value);
+
+  if (localStorage.getItem(key) !== '') {
+    formEl.value = localStorage.getItem(key);
+  } else {
+    return;
+  }
+}
 
 function onInputValue(evt) {
-  evt.preventDefault();
   const task = {};
   new FormData(evt.currentTarget).forEach((value, name) => {
     task[name] = value;
   });
+  console.log(task);
   return task;
 }
+
+const key = 'feedback-form-state';
+
+function localStorFn(data) {
+  const localValue = JSON.parse(localStorage.getItem(key)) || [];
+  localValue.push(data);
+  localStorage.setItem(key, JSON.stringify(localValue));
+}
+
+// function getLocalValue() {
+//   if (localStorage.getItem() !== '') {
+//     return localStorage.get(key);
+//   }else{
+
+//   }
+// }
